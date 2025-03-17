8fdb967ed2476ec0f6e9756e28f9d3041e00e3b0
PimSomsak <pym.somsak@gmail.com>
2025-03-17 09:48:14 +0700

    Cube rotation

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] Vector3 angularV, torqe;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.angularVelocity = angularV; 
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            rb.AddTorque(torqe);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            rb.angularVelocity = Vector3.zero;
+            rb.AddTorque(Vector3.zero);
+
+        }
+
     }
 }
