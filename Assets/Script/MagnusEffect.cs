5e1fb4529f52278ca22e546112c3595b6895c579
PimSomsak <pym.somsak@gmail.com>
2025-03-17 10:23:26 +0700

    Final

---
+++ Assets/Script/MagnusEffect.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] Vector3 velocity, spin;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Kick();
+        }
+
+        ApplyMagnusEffect();
+    }
+   
+
+    void Kick()
+    {
+        rb.linearVelocity = velocity;
+        rb.angularVelocity = spin;
+    }
+
+
+    void ApplyMagnusEffect()
+    {
+        Vector3 magnusForce = Vector3.Cross(rb.linearVelocity, rb.angularVelocity);
+        rb.AddForce(magnusForce, ForceMode.Force);
+    }
+}
